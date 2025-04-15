4c93efaa7aceddb6d4e122fd9aeb8901325c4916
samiksha818 <112892826+samiksha818@users.noreply.github.com>
2025-04-15 19:42:42 +0530

    Add files via upload

---
+++ Employee-Management-System-main/src/Welcome.java
@@ -0,0 +1,49 @@
+package employee.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Welcome extends JFrame implements ActionListener {
+    
+    Welcome() {
+        
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("EMPLOYEE MANAGEMENT SYSTEM");
+        heading.setBounds(60, 70, 1200, 80);
+        heading.setFont(new Font("italic", Font.BOLD, 60));
+        heading.setForeground(Color.BLACK);
+        add(heading);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/welcome.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(1400, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 1400, 800);
+        add(image);
+        
+        JButton clickhere = new JButton("CLICK HERE TO CONTINUE");
+        clickhere.setBounds(450, 400, 300, 70);
+        clickhere.setBackground(Color.BLACK);
+        clickhere.setForeground(Color.WHITE);
+        clickhere.addActionListener(this);
+        image.add(clickhere);
+        
+        
+        setSize(1170, 650);
+        setLocation(60, 30);
+        setVisible(true);
+        
+        }
+    
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Login();
+    }
+    
+    public static void main(String args[]) {
+        new Welcome();
+    }
+}
